3c5f6ab95e33bb79dfd512ab1ee18942804ffd45
Muhammad Shoaib Raza <154362378+ShoaibRaza12@users.noreply.github.com>
2025-04-11 02:18:54 -0700

    Add js/script.js 1744363133785

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// JavaScript for smooth scrolling and other functionalities
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Add any additional JavaScript functionality here
